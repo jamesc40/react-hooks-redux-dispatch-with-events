dad42cd8d088554ca05950e14ad6ce1b81ff6a2e
James Coffman <jamesc4023@gmail.com>
2022-06-16 18:46:42 -0400

    commit tree bca8a053443d5d2988a07316b74e9e72542fa9cf

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,28 @@
-// add code snippets from README
+let state;
+
+const reducer = (state = { count: 0 }, action) => {
+  switch(action.type){
+    case('counter/increment'):
+      return { count: state.count + 1 }
+    default:
+      return state
+   }
+}
+
+const dispatch = (action) => {
+  state = reducer(state, action)
+  render()
+}
+
+const render = () => {
+  const container = document.querySelector('#container')
+  container.textContent = state.count
+}
+
+const button = document.querySelector('#button')
+
+button.addEventListener('click', () => {
+  dispatch({ type: 'counter/increment' })
+})
+
+dispatch({ type: '@@INIT' })
